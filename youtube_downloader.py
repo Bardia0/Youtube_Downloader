c6d4d35bffaacd79b9725eaf96f106e1e86be56f
Bardia0 <90533995+Bardia0@users.noreply.github.com>
2024-02-04 07:20:38 +0330

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -28,8 +28,7 @@ def download_video(url, quality=None, playlist=False):
             yt = YouTube(video_url, on_progress_callback=on_progress)
             print(f"Downloading: {yt.title}...")
             
-            video_resolution = f"{quality}p" if quality else None
-            video = yt.streams.filter(res=video_resolution, file_extension='mp4').first()
+            video = yt.streams.filter(res=quality, file_extension='mp4').first()
             if video:
                 video.download(filename=yt.title, output_path=os.getcwd())
                 print("Download complete!")
